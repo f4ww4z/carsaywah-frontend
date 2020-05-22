f443def75d537c55f17e04333c0f9743d460bade
Maharaj Fawwaz Almuqaddim Yusran <fawwazyusran@gmail.com>
2020-05-22 09:54:29 +0800

    Change to Material UI instead

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
 import HomePage from './home/HomePage'
+import Navbar from './Navbar'
 
 function App () {
   return (
     <>
+      <Navbar/>
       <Switch>
         <Route path="/">
           <HomePage/>
--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { APP_NAME } from '../StringConstants'
 
 const HomePage = () => {
+
+  useEffect(() => {
+    document.title = `Home | ${APP_NAME}`
+  }, [])
+
   return (
     <div>
       Welcome to CarSayWah
